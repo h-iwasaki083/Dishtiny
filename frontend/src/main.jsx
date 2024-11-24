0261756657fa79f2d27b25b7d17199ca303a6f52
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2024-11-24 10:10:26 +0900

    frontend ChakraUIのインストール

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
+  </StrictMode>
+);
