b5e38581dfaea2a4e44a1f704cc8716f6ddb3042
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2024-11-28 01:05:11 +0900

    frontend chakraUI削除

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from './components/ui/provider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
       <Router>
         <App />
       </Router>
-    </Provider>
   </StrictMode>
 );
