637ba1b8d4241c9133b46d4ab8f553daa3725091
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2024-11-28 16:28:37 +0900

    frontend infoPageに必要なデータの受け渡し・表示〇

--- frontend/src/pages/Info.tsx
+++ frontend/src/pages/Info.tsx
@@ -1,19 +1,39 @@
-import { Button } from '@/components/ui/button';
-import { useState, useEffect } from 'react';
+// import { Button } from '@/components/ui/button';
+import { useEffect } from 'react';
+import useStore from '@/zustand/Store';
 
 const InfoPage = () => {
-    const [message, setMessage] = useState('');
-    useEffect(() => {
-      fetch('http://localhost:5000/')
-        .then((res) => res.text())
-        .then((data) => setMessage(data));
-    }, []);
+  // const [data, setData] = useState([]);
+  const { ingredients, setIngredients } = useStore();  // ZustandのストアからnumberとsetNumberを取得
+
+  useEffect(() => {
+    fetch('http://localhost:5000/info')
+      .then((res) => res.json())
+      .then((data) => {
+        setIngredients(data);  // バックエンドから受け取ったデータでingredientsを更新
+      });
+  }, [setIngredients]);
 
   return (
     <div>
       <h1>Example Page</h1>
-        <p>{message}</p>
-      <Button>ボタン</Button>
+        <p>{ingredients[1]?.name}</p>
+      <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+        <thead>
+          <tr>
+            <th>食材</th>
+            <th>料金</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ingredients.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- frontend/src/zustand/Store.tsx
+++ frontend/src/zustand/Store.tsx
@@ -5,11 +5,15 @@ import { create } from 'zustand';
 type Store = {
   number: number;
   setNumber: (newNumber: number) => void;
+  ingredients: string[];
+  setIngredients: (newIngredients: string[]) => void;
 };
 
 const useStore = create<Store>((set) => ({
   number: 3,
   setNumber: (newNumber) => set({ number: newNumber }),  // numberを更新する関数
+  ingredients: ['flour', 'sugar', 'eggs'],
+  setIngredients: (newIngredients) => set({ ingredients: newIngredients }),
 }));
 
 export default useStore;
